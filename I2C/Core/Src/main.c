a3a0d8c940c6f82ab9712c6bbf84be6e22aaf90a
MinhTrungK21 <voleminhtrung@gmail.com>
2025-05-31 22:39:08 +0700

    GPIO

--- I2C/Core/Src/main.c
+++ I2C/Core/Src/main.c
@@ -1,250 +1,128 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include"main.h"
+#include"stdio.h"
+#include"string.h"
+#define GPIOB_BASE_ADDR 0x40020400
+#define I2C_BASE_ADDR 	0x40005400
+uint32_t* GPIOB_MODER = (uint32_t*)(GPIOB_BASE_ADDR+0x00);
+uint32_t* GPIOB_AFRL  = (uint32_t*)(GPIOB_BASE_ADDR+0x20);
+uint32_t* GPIOB_AFRH  = (uint32_t*)(GPIOB_BASE_ADDR+0x24);
+uint32_t* I2C_CR1     = (uint32_t*)(I2C_BASE_ADDR+0x00);
+uint32_t* I2C_CR2     = (uint32_t*)(I2C_BASE_ADDR+0x04);
+uint32_t* I2C_CCR     = (uint32_t*)(I2C_BASE_ADDR+0x1C);
+uint32_t* I2C_SR1     = (uint32_t*)(I2C_BASE_ADDR+0x14);
+uint32_t* I2C_SR2     = (uint32_t*)(I2C_BASE_ADDR+0x18);
+uint32_t* I2C_DR      = (uint32_t*)(I2C_BASE_ADDR+0x10);
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+void I2C_Init()
+{
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-/* USER CODE END Includes */
+	*GPIOB_MODER &=~(0b11<<12);
+	*GPIOB_MODER |= (0b10<<12);
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+	*GPIOB_MODER &=~(0b11<<18);
+	*GPIOB_MODER |= (0b10<<18);
 
-/* USER CODE END PTD */
+	*GPIOB_AFRL  &=~(0b1111<<24);
+	*GPIOB_AFRL  |= (0b0100<<24);
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+	*GPIOB_AFRH  &=~(0b1111<<4);
+	*GPIOB_AFRH  |= (0b0100<<4);
 
-/* USER CODE END PD */
+	__HAL_RCC_I2C1_CLK_ENABLE();
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
+	*I2C_CR2     &=~(0b111111<<0);
+	*I2C_CR2     |= (16<<0);			//16mHZ
 
-/* USER CODE END PM */
+	*I2C_CCR 	 &=~(0xfFf<<0);
+	*I2C_CCR	 |=(160<<0);			//16MHZ/160=100KHZ
 
-/* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
+	*I2C_CR1 	 |=(1<<0);		//enable I2C1
+}
+void I2C_Write(uint8_t Slave_Reg_Addr, uint8_t Slave_Reg_Value)
+{
+	//Genarate StartBit
+	*I2C_CR1 |= (1<<8);
+	//wait Startbit SR1
+	while (((*I2C_SR1>>0)&1)==0);
+	//Send 7bit Slaveaddress + 1 bit write
+	*I2C_DR = 0x32;
+	while (((*I2C_SR1>>1)&1)==0);
+	uint32_t temp = *I2C_SR2;
+	//chekc ack
+	while(((*I2C_SR1>>10)&1)==1);
+	// set command frame (0x1F)
+	*I2C_DR = Slave_Reg_Addr;
 
-/* USER CODE BEGIN PV */
+	while(((*I2C_SR1>>2)&1)==0);
+	//check ack
+	while(((*I2C_SR1>>10)&1)==1);
+	//sent write data
+	*I2C_DR = 0b00110011;
+	while(((*I2C_SR1>>2)&1)==0);
+	//gererate stopbit
+	*I2C_CR1 |= (1<<9);
 
-/* USER CODE END PV */
+}
 
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
+uint16_t I2C_Read(uint8_t Slave_Reg_Addr)
+{
+	//Genarate StartBit
+	*I2C_CR1 |= (1<<8);
+	while (((*I2C_SR1>>0)&1)==0);
+	//send 7 bit slave address + 1 bit write
+	*I2C_DR = 0b00110011;
+	while (((*I2C_SR1>>1)&1)==0);
+	uint32_t temp = *I2C_SR2;
+	//chekc ack
+	while(((*I2C_SR1>>10)&1)==1);
+	//send command frame
+	*I2C_DR= Slave_Reg_Addr;
+	while(((*I2C_SR1>>2)&1)==0);
+	//chekc ack
+	while(((*I2C_SR1>>10)&1)==1);
+	//generate start bit
+	*I2C_CR1 |= (1<<8);
+	while (((*I2C_SR1>>0)&1)==0);
+	//send 7 bit slaveaddress+ 1 bit read
+	*I2C_DR = 0b00110011;
+	while (((*I2C_SR1>>1)&1)==0);
+	temp = *I2C_SR2;
+	//check ack
+	while(((*I2C_SR1>>10)&1)==1);
+	//read data
+	while(((*I2C_SR1>>6)&1)==0);
+	uint8_t data = *I2C_DR;
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-int16_t x,y,z;
-/* USER CODE END 0 */
+	// generate stopbit
+	*I2C_CR1 |= (1<<9);
+	return data;
+}
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
+int16_t Read_Accel_X()
+{
+    uint8_t low  = I2C_Read(0x28);  // OUT_X_L_A
+    uint8_t high = I2C_Read(0x29);  // OUT_X_H_A
+
+    int16_t x = (int16_t)((high << 8) | low);
+    return x;
+}
+int16_t accel_x;
 int main(void)
 {
+    HAL_Init();
+    I2C_Init();
+    I2C_Write(0x20, 0x57);
 
-  /* USER CODE BEGIN 1 */
+    while (1)
+    {
+        uint8_t xl = I2C_Read(0x28);
+        uint8_t xh = I2C_Read(0x29);
+        accel_x = (int16_t)((xh << 8) | xl);
 
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */
-  uint8_t dev_id=0;
-  uint8_t ctrl_1_val=0b01010111;
-  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x0f , 1 , &dev_id, 1, HAL_MAX_DELAY);
-  HAL_I2C_Mem_Write(&hi2c1, 0b0011001<<1, 0x20, 1, &ctrl_1_val, 1, HAL_MAX_DELAY);
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-	  uint8_t low,high;
-
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x28 , 1 , &low, 1, HAL_MAX_DELAY);
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x29 , 1 , &high, 1, HAL_MAX_DELAY);
-	  x = (high<<8)|low;
-
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x2A , 1 , &low, 1, HAL_MAX_DELAY);
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x2B , 1 , &high, 1, HAL_MAX_DELAY);
-	  y = (high<<8)|low;
-
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x2C , 1 , &low, 1, HAL_MAX_DELAY);
-	  HAL_I2C_Mem_Read(&hi2c1, 0b0011001<<1,0x2D , 1 , &high, 1, HAL_MAX_DELAY);
-	  y = (high<<8)|low;
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+        HAL_Delay(500);
+    }
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
-}
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
--- UART/Core/Src/main.c
+++ UART/Core/Src/main.c
@@ -1,17 +1,20 @@
+
 /*
  * main.c
  *
  *  Created on: Mar 23, 2025
  *      Author: Dell
  */
-#include "main.h"
+#include"main.h"
 #include"stdio.h"
+#include"string.h"
 #define GPIOD_BASE_ADDR  0x40020C00
 #define GPIOA_BASE_ADDR  0x40020000
 #define RCC_BASE_ADDR    0x40023800
 #define EXTI_BASE_ADDR    0x40013C00
 #define SYSCFG_BASE_ADDR  0x40013800
 #define NVIC_ISER0        ((volatile uint32_t*)0xE000E100)  // Interrupt Set-Enable Register
+#define NVIC_ISER1        ((volatile uint32_t*)0xE000E104)
 #define UART1_BASE_ADDR 0x40011000
 #define GPIOB_BASE_ADDR 0x40020400
 uint32_t* RCC_AHB1ENR  = (uint32_t*)(RCC_BASE_ADDR + 0x30);
@@ -22,6 +25,7 @@ uint32_t* GPIOA_IDR    = (uint32_t*)(GPIOA_BASE_ADDR + 0x10);
 uint32_t* GPIOA_PUPDR  = (uint32_t*)(GPIOA_BASE_ADDR + 0x0C);
 uint32_t* GPIOA_MODER  = (uint32_t*)(GPIOA_BASE_ADDR + 0x00);
 uint32_t* GPIOD_ODR    = (uint32_t*)(GPIOA_BASE_ADDR + 0x014);
+uint32_t* GPIOB_MODER  = (uint32_t*)(GPIOB_BASE_ADDR);
 uint8_t   led_state    = 0;
 
 // Thanh ghi EXTI
@@ -32,7 +36,10 @@ uint32_t* EXTI_PR   = (uint32_t*)(EXTI_BASE_ADDR + 0x14);
 
 // SYSCFG
 uint32_t* SYSCFG_EXTICR1 = (uint32_t*)(SYSCFG_BASE_ADDR + 0x08);
-
+//FLASH
+uint32_t* FLASH_SR 	 =  (uint32_t*)(0x40023C00+0x0C);
+uint32_t* FLASH_CR   =  (uint32_t*)(0x40023C00+0x10);
+uint32_t* FLASH_KEYR =  (uint32_t*)(0x40023C00+0x04);
 typedef enum
 {
 	Green_LED  =12,
@@ -62,8 +69,18 @@ void LedCtrl(LED_t led, int LED_stt)
 	}
 #endif
 }
-
-
+char ButtonState()
+{
+	uint32_t* GPIOA_IDR = (uint32_t*)(GPIOA_BASE_ADDR + 0x10);
+	return (*GPIOA_IDR >> 0) & 1;
+}
+void function()
+{
+	if(ButtonState())
+		LedCtrl(Red_LED, 1);
+	else
+		LedCtrl(Red_LED, 0);
+}
 void LedInit()
 {
 
@@ -76,8 +93,18 @@ void LedInit()
 
 
 
-void ButtonInit()
+void EXTI0Init()
 {
+	/*
+	- Làm sao để EXTI0 gửi interrupt signal lên ARM?
+		+ chọn cạnh (rising/falling/cả hai)
+			+ set trong thanh ghi EXTI_RTSR và EXTI_FTSR
+		+ enable exti0(set mark)
+			+ set trong thanh ghi EXTI_IMR
+	- ARM (NVIC) phải chấp nhận interrupt signal từ EXTI gửi lên?
+		+ bước 1: xác định EXTI0 nằm ở position bao nhiêu trong vector table? (mở vector table ở chapter "10: interrupts and events" trong reference manual) --> 6
+		+ bước 2: enable interrupt cho position 6
+	*/
 	*RCC_AHB1ENR  |= (1 << 0);   // GPIOA
 	*RCC_APB2ENR  |= (1 << 14);  // SYSCFG
 
@@ -95,37 +122,67 @@ void ButtonInit()
 
     //cho phep NVIC nhan ngat tu pa0
     *NVIC_ISER0 |= (1 << 6);
+    // Move vector table lên RAM (0x20000000)
+    	uint8_t* src = 0;
+    	uint8_t* dis = (uint8_t*)0x20000000;
+    	for(int i = 0; i < 0x198; i++)
+    	{
+    		//dis[i] = src[i];
+    		*(dis+i) = *(src+i);
+    	}
+    	//Báo ARM vector table đã được offset lên RAM
+    	uint32_t* VTOR = (uint32_t*)0xE000ED08;
+    	*VTOR = 0x20000000;
+    	//
+    	int* ptr;
+    	ptr= (int*)0x20000058;
+    	*ptr = (int)function;
+
 }
 
 
 // Hàm xử lý ngắt EXTI0
+
 void EXTI0_IRQHandler(void)
 {
-	if (*EXTI_PR & (1 << 0))
-	{
-		*EXTI_PR |= (1 << 0);  // Clear pending
+	if(ButtonState())
+		LedCtrl(Red_LED, 1);
+	else
+		LedCtrl(Red_LED, 0);
 
-		if (*GPIOA_IDR & (1 << 0))
-			LedCtrl(Red_LED, 1);
-		else
-			LedCtrl(Red_LED, 0);
-	}
+	//clear interrupt flag
+	uint32_t* EXTI_PR = (uint32_t*)(EXTI_BASE_ADDR + 0x14);
+	*EXTI_PR |= (1<<0);
 }
 void UART_Init()
 {
+
 	__HAL_RCC_GPIOB_CLK_ENABLE();
 	uint32_t* GPIOB_MODER = (uint32_t*)(GPIOB_BASE_ADDR + 0X00);
 	*GPIOB_MODER &=(0b1111<12);
 	*GPIOB_MODER |=(0b10<<12)|(0b10<<14);
+
+
 	uint32_t* GPIOB_AFLR = (uint32_t*)(GPIOB_BASE_ADDR+0x20);
 	*GPIOB_AFLR &=~(0xff<24);
 	*GPIOB_AFLR |=(0b0111<<24)|(0b0111<<28);
+
+
 	__HAL_RCC_USART1_CLK_ENABLE(); //16MHz
 	uint32_t* BRR = (uint32_t*)(UART1_BASE_ADDR+0x08);
 	*BRR = (104<<4)|(3<<0);
 	uint32_t* CR1 = (uint32_t*)(UART1_BASE_ADDR+0x0C);
 	*CR1 |= (1<<12)|(1<<10)|(1<<3)|(1<<2)|(1<<13);
+
+
+	/* enable interrupt*/
+	*CR1 |= (1<<5);
+	*NVIC_ISER1 |= (1 << 5);
+
+
 }
+
+
 
 void UART_Transmit(uint8_t data)
 {
@@ -137,6 +194,15 @@ void UART_Transmit(uint8_t data)
 	while (((*SR>>6)&1)==0);
 
 }
+void UART_Print_Log(char* msg)
+{
+	int msg_len =  strlen(msg);
+	for (int i =0; i<msg_len; i++)
+	{
+
+		UART_Transmit((uint8_t)msg[i]);
+	}
+}
 char UART_Receive()
 {
 	uint32_t* DR = (uint32_t*)(UART1_BASE_ADDR+0x04);
@@ -145,16 +211,29 @@ char UART_Receive()
 	char data=*DR;
 	return data;
 }
-int main(void)
+char StoreData[100];
+int idx;
+
+void USART1_IRQHandler()
+{
+	StoreData[idx++]= UART_Receive();
+
+}
+
+
+int main()
 {
 	HAL_Init();
 	LedInit();
-	ButtonInit();
+	EXTI0Init();
 	UART_Init();
+
+
 	while (1)
 	{
+
 		UART_Transmit('x');
-		HAL_Delay(1000);
+		char data=UART_Receive();
 		if(UART_Receive()=='a')
 		{
 			LedCtrl(Red_LED, 1);
@@ -163,7 +242,10 @@ int main(void)
 		{
 			LedCtrl(Red_LED, 0);
 		}
-
+		LedCtrl(Blue_LED,1);
+		HAL_Delay(1000);
+		LedCtrl(Blue_LED,0);
+		HAL_Delay(1000);
 	}
 	return 0;
 }
